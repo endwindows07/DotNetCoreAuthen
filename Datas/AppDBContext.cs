e0bdd751acc86c87b917be368f81a069abbf4d66
Pramot Samranwong <endwindows07@gmail.com>
2019-10-29 12:50:03 +0700

    add custom identity test

--- Datas/AppDBContext.cs
+++ Datas/AppDBContext.cs
@@ -19,4 +19,29 @@ namespace netcoreAuthen.Datas
             builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
         }
     }
+
+    // การเพิ่ม Custom User Identity
+    public class AppDBContextCustom : IdentityDbContext<CustomUserApp>
+    {
+        public AppDBContextCustom(DbContextOptions<AppDBContextCustom> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // create mock role data
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "User", NormalizedName = "USER", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "Admin", NormalizedName = "ADMIN", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
+        }
+    }
+
+
+
+    // การทำ Custom Identity User
+    public class CustomUserApp : IdentityUser
+    {
+        //เพิ่ม property ที่ยต้องการ
+
+    }
 }
